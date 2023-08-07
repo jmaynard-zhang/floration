7eaf31de629057d09635c0cb91501ac532dc586d
Jasper Maynard-Zhang <maynjas19@gmail.com>
2023-08-07 12:35:07 -0700

    Updates

---
+++ data/convertCSVtoJSON.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const Papa = require('papaparse');
+
+const csvFilePath = './public/data/plants.csv';
+const jsonDataPath = './public/data/plants.json';
+
+fs.readFile(csvFilePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error('Error reading CSV file:', err);
+    return;
+  }
+
+  const jsonData = Papa.parse(data, { header: true, skipEmptyLines: true });
+
+  fs.writeFile(jsonDataPath, JSON.stringify(jsonData.data, null, 2), (err) => {
+    if (err) {
+      console.error('Error writing JSON file:', err);
+      return;
+    }
+
+    console.log('CSV to JSON conversion successful!');
+  });
+});
---
+++ data/plants.js
@@ -0,0 +1,26 @@
+const plants = [
+    {
+      id: 1,
+      name: 'Rose',
+      kingdom: 'Plantae',
+      habitat: 'Garden',
+      imgId: '1zgxOKnZhKQ3fGLGHjNonAle9VJKRFdRI'
+    },
+    {
+      id: 2,
+      name: 'Fern',
+      kingdom: 'Plantae',
+      habitat: 'Forest',
+      imgId: '1nsKEb30va5H6erV7NdgkUvdap_wMuh1R'
+    },
+    {
+      id: 3,
+      name: 'Cactus',
+      kingdom: 'Plantae',
+      habitat: 'Desert',
+      imgId: '1_bo5kqkdtXAepdwC8iFrM93-UvszlOIz'
+    },
+    // Add more plants with different data as needed
+  ];
+
+export default plants;
